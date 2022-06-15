94c6ec6fbbb880693684538c4a2f0bfd82254a5d
Duprat <yduprat@gmail.com>
2022-06-15 14:00:47 +0200

    gitautosync update 62

--- Kampus/Documents&Exemples/model.py
+++ Kampus/Documents&Exemples/model.py
@@ -1,8 +1,12 @@
 # -------- PART I: IMPORT + CONSTANTS ------------
 import sys
 FILENAME, EXTENSION = 0, 1
+results = []
 
-# ---------- PART II ALL FUNCTIONS ----------
+# ---------- PART II ALL FUNCTIONS & CLASSES----------
+class Test:
+    pass
+
 def function1():
     """
     """
@@ -16,5 +20,5 @@ def main():
 # ------------- PART III: MAIN ENTRY ----------
 if __name__ == "__main__":
     print(sys.version)
-    print("name est", __name__)
+    print("name est", __name__, f"file is {__file__}")
     main()
