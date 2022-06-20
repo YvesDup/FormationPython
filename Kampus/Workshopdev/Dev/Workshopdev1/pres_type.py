11efc1d1c8114a8543ddc998b05ac44769468839
Duprat <yduprat@gmail.com>
2022-06-20 15:26:23 +0200

    gitautosync update 14

--- Kampus/Workshopdev/Dev/Workshopdev1/pres_type.py
+++ Kampus/Workshopdev/Dev/Workshopdev1/pres_type.py
@@ -4,9 +4,12 @@ import sys
 def type_simple():
     """Présentation des types simples
     """
+    print("entiers")
     a = 10 # a est un entier
     al = 10_000_000_000_000_000
     print(a)
     print(al)
+    b = a 
+    print(b)
 
 type_simple()
