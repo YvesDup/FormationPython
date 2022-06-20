04ea96535df0b27ffaf8bddcd1f6be05cde5a27e
Duprat <yduprat@gmail.com>
2022-06-20 17:37:39 +0200

    gitautosync update 69

---
+++ Kampus/Workshopdev/Dev/Workshopdev1/anagramme.py
@@ -0,0 +1,23 @@
+def anagramme(words):
+	"""  words contient une liste de mot
+	"""
+	res = []
+
+
+
+	# renvoie les 
+	return res
+
+
+def test_anagramme():
+	"""
+	"""
+	tests = (
+			(("ecrue", "uni", nue") , 	[])
+			(("les", "sel", "sell"), 	[["les", "sel"])
+			(("line", "steres", "stress", "tres", lien", "restes"), 	[["lien", "line"], [["restes", "steres", "stress"])
+	)
+	for words, res in :
+		print(anagramme(words) == res) 
+
+test_anagramme()
