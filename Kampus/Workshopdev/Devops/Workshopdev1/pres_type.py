986f137e3fbd3643f6947ce876095842afbbce3f
Duprat <yduprat@gmail.com>
2022-06-21 11:13:01 +0200

    gitautosync update 107

--- Kampus/Workshopdev/Devops/Workshopdev1/pres_type.py
+++ Kampus/Workshopdev/Devops/Workshopdev1/pres_type.py
@@ -4,32 +4,39 @@ import sys
 def type_simple():
     """Présentation des types simples
     """
-    print("entiers")
-    a = 10 # a est un entier
-    al = 10_000_000_000_000_000
-    print(a)
-    print(al)
-    b = a 
-    print(f'{b = }')
-    print(f'{id(a) = }, {id(b) =}')
-    print('je modifie b'.center(60, '-'))
-    b += 10
-    print(f'{b = }')
-    print(f'{id(a) = }, {id(b) =}')
+    def type_entier():
+        print("entiers")
+        a = 10 # a est un entier
+        al = 10_000_000_000_000_000
+        print(a)
+        print(al)
+        b = a 
+        print(f'{b = }')
+        print(f'{id(a) = }, {id(b) =}')
+        print('je modifie b'.center(60, '-'))
+        b += 10
+        print(f'{b = }')
+        print(f'{id(a) = }, {id(b) =}')
 
-    print('flottant')
-    f = 0.5
-    print(f'{f=}')
-    fl = f
-    print(f'{int(f)}')
+    def type_float():
+        print('flottant')
+        f = 0.5
+        print(f'{f=}')
+        fl = f
+        print(f'{int(f)}')
 
-    print("booléen")
-    bot = True
-    bof = False
-    print(f'{bot = }, {bof = }')
-    print("complexe")
-    cplx = 2j + 9
-    print(cplx)
+    def type_bool():
+        print("booléen")
+        bot = True
+        bof = False
+        print(f'{bot = }, {bof = }')
+
+    def type_cplx():
+        print("complexe")
+        cplx = 2j + 9
+        print(cplx)
+
+
     print("valeur nulle")
     n = None
     print(f'{n = } - {type(None)}')
