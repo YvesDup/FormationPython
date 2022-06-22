79e0c8daa33cec6b52a5fee46192fb1481b37da5
Duprat <yduprat@gmail.com>
2022-06-22 15:51:26 +0200

    gitautosync update 241

--- Kampus/Workshopdev/Archi/Workshopdev1/model_exo.py
+++ Kampus/Workshopdev/Archi/Workshopdev1/model_exo.py
@@ -1,16 +1,8 @@
-import sys
 
 def exo2():
-    """Exercice XX
+    """Exercice 2 - ceci est une docstring 
     """
-    res = 3 + 234
+    res = 2 + 100
     print(f'1 - {res = }')
 
-    fres = res * 3.14159
-    print(f'2 - {fres = } , {type(fres) = }')
-
-    print(f'3 - {1 + True = }')
-
-    print(f'{10.4 / None}')
-
-exo2()
+exo2() 
---
+++ Kampus/Workshopdev/Devops/Workshopdev1/model_exo_old.py
@@ -0,0 +1,16 @@
+import sys
+
+def exo2():
+    """Exercice XX
+    """
+    res = 3 + 234
+    print(f'1 - {res = }')
+
+    fres = res * 3.14159
+    print(f'2 - {fres = } , {type(fres) = }')
+
+    print(f'3 - {1 + True = }')
+
+    print(f'{10.4 / None}')
+
+exo2()
