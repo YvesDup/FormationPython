90e6fd74b6091c9c4def8751fdaa0bb243e32d5a
Duprat <yduprat@gmail.com>
2022-06-22 15:58:01 +0200

    gitautosync update 244

--- Kampus/Workshopdev/Archi/Workshopdev1/model_exo.py
+++ Kampus/Workshopdev/Archi/Workshopdev1/model_exo.py
@@ -8,4 +8,11 @@ def exo2():
     fres = 3.14159 * res
     print(f'2 - {fres = }')
 
+    x = True + 1
+    print(f'3 - {x = }')
+
+    print(f'5 - {type(None) = }')
+
+    print(f'4 - {10.4 / None}')
+
 exo2() 
