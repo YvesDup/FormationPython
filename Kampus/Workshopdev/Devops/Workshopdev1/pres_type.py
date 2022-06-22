faff7cd4638dec104513aaead9cc890c06f911b7
Duprat <yduprat@gmail.com>
2022-06-22 17:18:29 +0200

    gitautosync update 258

--- Kampus/Workshopdev/Devops/Workshopdev1/pres_type.py
+++ Kampus/Workshopdev/Devops/Workshopdev1/pres_type.py
@@ -110,6 +110,7 @@ def type_avance():
     """
     """
     def type_liste():
+        """
         # liste
         print('liste')
         l = []
@@ -154,7 +155,7 @@ def type_avance():
         print(l1)
         l1.sort()
         print(l1)
-        return
+        """
 
         print("append a list")
         l1 = [-5, -1.2e-19, 0, True, 2.89, 10]
