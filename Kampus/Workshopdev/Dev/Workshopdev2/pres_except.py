c2593215ee6e1bdac1d8ca9fb29fa0fe4e9b22b2
Duprat <yduprat@gmail.com>
2022-06-28 18:08:50 +0200

    gitautosync update 18

--- Kampus/Workshopdev/Dev/Workshopdev2/pres_except.py
+++ Kampus/Workshopdev/Dev/Workshopdev2/pres_except.py
@@ -7,13 +7,16 @@ def pres_except():
     """
     try:
         1/0
-    except:
-        pass
-    except:
-        pass
-
+    except ZeroDivisionError as e:
+        print(f'{type(e)} - {str(e)}')
+    except Exception as e:
+        print(f'{type(e)} - {str(e)}')
+    except BaseException as e:
+        print("baseException")
     else:
         print("on passe ici qaud il n'y apas d'erreur")
 
     finally:
         print("on passe tout le temps la ....")
+
+pres_except()
