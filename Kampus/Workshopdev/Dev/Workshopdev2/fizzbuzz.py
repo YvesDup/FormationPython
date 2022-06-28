0abceb4755e629062c27df4ace1b369106f586bd
Duprat <yduprat@gmail.com>
2022-06-28 16:56:34 +0200

    gitautosync update 2

--- Kampus/Workshopdev/Dev/Workshopdev2/fizzbuzz.py
+++ Kampus/Workshopdev/Dev/Workshopdev2/fizzbuzz.py
@@ -5,7 +5,13 @@ def fizzbuzz(n):
     """
     results = []
     for value in range(1, n+1):
-        print(value)
+        if value % 5 == 0 and value % 3 == 0:
+        # if value % 15 == 0:
+            results.append('fizzbuzz')
+        elif value % 5 == 0:
+            results.append('buzz')
+        elif value % 3 == 0:
+            results.append('fizz')
 
     return results
 
@@ -14,5 +20,6 @@ def test_fizzbuzz():
     """
     assert fizzbuzz(0) ==  []
     assert fizzbuzz(3) ==  [1, 2, "fizz"]
+    assert fizzbuzz(1000).count('fizzbuzz') == 1000//15  
 
 test_fizzbuzz()
