c603cc5a5011af1b7d92e84d3d9445c195a8a5a0
Duprat <yduprat@gmail.com>
2022-06-28 17:28:18 +0200

    gitautosync update 9

---
+++ Kampus/Workshopdev/Dev/Workshopdev2/anaalyse_texte.py
@@ -0,0 +1,19 @@
+
+
+
+def analyse_texte(*args):
+    """
+    """
+    pluscourte_chaine = ""
+    pluslongue_chaine = ""
+    moyenne_longueur = 0.0
+
+
+    return pluscourte_chaine, pluslongue_chaine, moyenne_longueur
+
+def test_analyse_texte():
+    """
+    """
+    assert analyse_texte('hello', 2.234, 't') == ('t', 'hello', 3.0)
+
+test_analyse_texte()
