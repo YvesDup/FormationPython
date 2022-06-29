aca7b0f77d3d41056ed69ce3c1e883e922f3db33
Duprat <yduprat@gmail.com>
2022-06-29 16:47:53 +0200

    gitautosync update 9

--- Kampus/Workshopdev/Devops/Workshopdev2/lit_while.py
+++ Kampus/Workshopdev/Devops/Workshopdev2/lit_while.py
@@ -5,6 +5,30 @@ def lit_tantque():
     """
     while True:
         valeur_tapee = input("tapez qlqchose:")
-        print(valeur_tapee)
+        print(f'{valeur_tapee = }')
+
+def lit_tantque_1():
+    """
+    """
+    valeur_tapee = ""
+    while valeur_tapee != "QQ":
+        valeur_tapee = input("tapez qlqchose:")
+        print(f'{valeur_tapee = }')
+
+def lit_tantque_2():
+    """
+    """
+    while True:
+        valeur_tapee = input("tapez qlqchose:")
+        if valeur_tapee == "QQ":
+            break
+        print(f'{valeur_tapee = }')
+        
+def lit_tantque_2():
+    """
+    """
+    while input("tapez qlqchose:") != "QQ":
+        pass
+        # print(f'{valeur_tapee = }')
 
 lit_tantque()
