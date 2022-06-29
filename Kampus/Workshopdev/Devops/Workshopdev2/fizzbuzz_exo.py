46c83ff067117547346e50bc93c4cb71df0cedab
Duprat <yduprat@gmail.com>
2022-06-29 16:54:10 +0200

    gitautosync update 10

---
+++ Kampus/Workshopdev/Devops/Workshopdev2/fizzbuzz_exo.py
@@ -0,0 +1,20 @@
+import sys
+
+def fizzbuzz(n):
+    """
+    """
+    results = []
+    for i in range(1, n+1):
+        print(i)
+        results.append(i)
+
+    return results
+
+def test_fizzbuzz(): 
+
+    print(fizzbuzz(0) == [])
+    print(fizzbuzz(3) == [1, 2, 'fizz'])
+    print(fizzbuzz(5) == [1, 2, 'fizz', 4, 'buzz'])
+    print(fizzbuzz(15)[14] == 'fizzbuzz')
+
+test_fizzbuzz()
