41f06de0e1a038166ce63ef22f81695af5598af1
Duprat <yduprat@gmail.com>
2022-06-29 18:02:43 +0200

    gitautosync update 16

---
+++ Kampus/Workshopdev/Devops/Workshopdev2/creer_index.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+
+def creer_index(mots):
+    """
+    """
+    dmots = {}
+
+
+
+
+    return dmots
+
+def test_creer_index():
+    """
+    """
+    print(creer_index("toto") == { "toto":1})
+    print(creer_index("toto toto") == { "toto":2})
+
+print(sys.version)
+print(os.getcwd())
+test_creer_index()
