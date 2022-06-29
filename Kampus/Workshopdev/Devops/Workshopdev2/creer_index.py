402bcd6dec1d9adbc34b1aeb2b9be6189ea455d9
Duprat <yduprat@gmail.com>
2022-06-29 20:58:26 +0200

    gitautosync update 27

--- Kampus/Workshopdev/Devops/Workshopdev2/creer_index.py
+++ Kampus/Workshopdev/Devops/Workshopdev2/creer_index.py
@@ -18,6 +18,8 @@ def creer_index(mots, min_occurence=1, debug=False):
 
     # filter
     if min_occurence > 1:
+        return {k:v for k, v in dmots if v > min_occurence}
+
         dd = {}
         for k, v in dmots.items():
             if v >= min_occurence:
