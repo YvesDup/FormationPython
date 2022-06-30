588905d77ef60db0e4dc328a5ae37c0bba335273
Duprat <yduprat@gmail.com>
2022-06-30 15:30:32 +0200

    gitautosync update 34

--- Kampus/Workshopdev/Archi/Workshopdev1/anagramme.py
+++ Kampus/Workshopdev/Archi/Workshopdev1/anagramme.py
@@ -2,10 +2,17 @@ def anagramme(words):
 	"""  words contient une liste de mot
 	"""
 	res = []
+	# il faut le même nombre de lettre
+	# le tri des lettres du mot et on va comparer avec le tri des lettres d'un autre mot
+	# creer un dictionnaire:
+	# key: le mot trié par lettre sous la forme: tuple(sorted(word))
+	# value: une liste de mots sous la forme [word1, word2, ...]
+	dword = {}
+	for word in words:
+		pass
 
+	# renvoie les values du dictionnaire si la longueur est supérieure à 1
 
-
-	# renvoie les 
 	return res
 
 
