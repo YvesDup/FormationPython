7f0f99677517ca800d8fc1220fbceb93b288baed
Duprat <yduprat@gmail.com>
2022-06-30 17:40:03 +0200

    gitautosync update 46

---
+++ Kampus/Workshopdev/Archi/Workshopdev2/analyse_text.py
@@ -0,0 +1,16 @@
+
+
+
+def analyse_texte(val, *args):
+    """
+    """
+
+
+    return "", "", 0.0
+
+def test_analyse_texte():
+    """
+    """
+    analyse_texte('hello', 2.234, 't') == 't', 'hello', 3.0
+
+test_analyse_texte()
