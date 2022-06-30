22f7c792b89079a5a661c3d8d621fbc30418e292
Duprat <yduprat@gmail.com>
2022-06-30 17:55:56 +0200

    gitautosync update 51

--- Kampus/Workshopdev/Archi/Workshopdev2/analyse_text.py
+++ Kampus/Workshopdev/Archi/Workshopdev2/analyse_text.py
@@ -1,16 +1,31 @@
-
-
+import sys
 
 def analyse_texte(val, *args):
     """
     """
+    lg_min = sys.maxsize
+    lg_max = 0
+    long_mot = ''
+    court_mot = ''
+    lg_total = 0
+    nb_mots = 0
     args = (val,) + args # pour respecter l'ordre
     for arg in args:
         if isinstance(arg, str):
-            pass
+            lg = len(arg)
+            if lg < lg_min:
+                court_mot = arg
+                lg_min = lg
+            if lg > lg_max:
+                long_mot = arg
+                lg_max = lg
+            nb_mots += 1
+            lg_total += lg
 
+    if nb_mots:
+        return court_mot, long_mot, lg_total/nb_mots
 
-    return "", "", 0.0
+    raise ValueError("Pas de string :-(")
 
 def test_analyse_texte():
     """
@@ -23,3 +38,14 @@ def test_analyse_texte():
     print(analyse_texte('hello', 2.234, 't') == ('t', 'hello', 3.0))
 
 test_analyse_texte()
+
+def analyse_texte_v2(val, *args):
+    args = (val,) + args # pour respecter l'ordre
+    mots = tuple(map(lambda x: isinstance(x, str), args))
+    if mots:
+        court_mot = min(mots, key=len)
+        long_mot = max(mots, key=len)
+        moyenne = "".join(mots) / len(mots)
+        return court_mot, long_mot, moyenne
+
+    raise ValueError("pas de string :-(")
