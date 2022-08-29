835f2e91d88e156687fcd0637929aef7232699d3
Duprat <yduprat@gmail.com>
2022-08-29 19:11:05 +0200

    gitautosync update 74

--- Kampus/Training/sample_annotations.py
+++ Kampus/Training/sample_annotations.py
@@ -5,9 +5,9 @@ def racine_carre(val):
     return math.sqrt(val)
 
 def main():
-    racine_carre(10)
-    racine_carre(10.169)
-    racine_carre("10")
+    print(racine_carre(10))
+    print(racine_carre(10.169))
+    print(racine_carre("10"))
 
 if __name__ == '__main__':
     main()
