68177c420d4c4ba66d8e33cb049573401a00ee28
Duprat <yduprat@gmail.com>
2022-08-29 21:47:03 +0200

    gitautosync update 87

--- Kampus/Training/sample_annotations.py
+++ Kampus/Training/sample_annotations.py
@@ -1,6 +1,6 @@
 import math
 import sys
-from typing import Union
+from typing import Union, List, Set, Tuple
 
 def racine_carre(val: Union[float, int]) -> float:
     """calcul de la racine carrée"""
@@ -11,5 +11,12 @@ def main():
     print(racine_carre(10.169))
     print(racine_carre("10"))
 
+
+Number = Union[float, int] # alias
+def minmax(serie: Union[List, Set, Tuple]) -> Tuple[Number, Number]:
+    """Renvoie le min et le max à partir d'une serie de chiffres"""
+    return min(serie), max(serie)
+
 if __name__ == '__main__':
     main()
+    print(minmax.__name__, '->', minmax.__annotations__, minmax.__doc__)
