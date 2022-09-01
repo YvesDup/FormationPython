11e80f20807b28055b7a43ba00e2c610b987f197
Duprat <yduprat@gmail.com>
2022-09-01 16:33:52 +0200

    gitautosync update 163

--- Kampus/Workshopdev/Dev/W3/exo20.py
+++ Kampus/Workshopdev/Dev/W3/exo20.py
@@ -10,10 +10,10 @@ class Parallelo:
         pass
 
     def __str__(self) -> str:
-        return ""
+        return f"{str hex(id(self)))}""
 
     def __repr__(self) -> str:
-        return ""
+        return f"{repr hex(id(self)))}"
 
     def perimetre(self) -> float:
         return 0.0
@@ -25,6 +25,6 @@ def test_parallelo():
     """fonction de test"""
     try:
         p = Parallelo(10, 20, 60)
-        print(p, p.surface(), p.surface())
+        print(p, p.surface(), p.perimetre())
     except Exception as e :
         print(e)
