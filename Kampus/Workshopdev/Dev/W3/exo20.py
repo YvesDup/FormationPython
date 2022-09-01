0ac0823425a6ee00307e5ca73130b4416831eb02
Duprat <yduprat@gmail.com>
2022-09-01 16:31:32 +0200

    gitautosync update 159

---
+++ Kampus/Workshopdev/Dev/W3/exo20.py
@@ -0,0 +1,30 @@
+"""Class parallelo - exo20"""
+import math
+import sys
+from xml.dom.pulldom import parseString
+
+
+class parallelo:
+    """Class Parallelo - exo20"""
+    def __init__(self, pc, gc, angle):
+        pass
+
+    def __str__(self) -> str:
+        return ""
+
+    def __repr__(self) -> str:
+        return ""
+
+    def perimetre(self) -> float:
+        return 0.0
+
+    def surface(self) -> float:
+        return 0.0
+
+def test_parallelo():
+    """fonction de test"""
+    try:
+        p = parallelo(10, 20, 60)
+        print(p, p.surface(), p.surface())
+    except:
+        parseString
