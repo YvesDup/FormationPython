a7388bd5bd22f36f4e97b9b85e0f13b8b9c206dd
Duprat <yduprat@gmail.com>
2022-09-01 18:09:25 +0200

    gitautosync update 192

--- Kampus/Workshopdev/Dev/W3/pres_herit.py
+++ Kampus/Workshopdev/Dev/W3/pres_herit.py
@@ -4,3 +4,19 @@ class A:
 print(dir(A))
 print(A.__mro__)
 print(A.__class__.__name__)
+
+class A:
+    pass
+class B(A): # j'hérite de A
+    pass
+class C(B): # j'hérite de B
+    pass
+
+print(B.__mro__)
+# renvoie un tuple qui contient toutes les classes héritées
+# directement ou via les classes héritées intermédiaires
+# ('<class B>', '<class A>', '<class object>')
+
+print(C.__bases__)
+# Renvoie un tuple qui contient les classes parentes
+# ('class B',)
