57f354717c352cdd2bd3485b3f725ec928b25b71
Duprat <yduprat@gmail.com>
2022-09-01 15:24:48 +0200

    gitautosync update 141

--- Kampus/Workshopdev/Dev/W3/sample_obj.py
+++ Kampus/Workshopdev/Dev/W3/sample_obj.py
@@ -1,12 +1,25 @@
 """ Presntation des objets en python"""
 
-class Obj:
-    """Presentation d'un objet simple"""
-    # pass
+def show_obj():
+    class Obj:
+        """Presentation d'un objet simple"""
+        # pass
 
+    o = Obj()
+    print(o)
 
-o = Obj()
-print(o)
+    o2 = Obj()
+    print(o == o2)
 
-o2 = Obj()
-print(o == o2)
+class Test:
+    """Classe avec arametre"""
+    def __init__(self, val):
+        self.val = val
+
+try:
+    t = Test() # ne fonctionne pas car un paramètre positionnel
+except Exception as e:
+    print(type(e), str(e))
+
+t = Test(10)
+print(t)
