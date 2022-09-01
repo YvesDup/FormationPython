3c2e4a4928115dded813d04e33246991759c444d
Duprat <yduprat@gmail.com>
2022-09-01 16:51:36 +0200

    gitautosync update 173

--- Kampus/Workshopdev/Dev/W3/exo20.py
+++ Kampus/Workshopdev/Dev/W3/exo20.py
@@ -8,9 +8,12 @@ class Parallelo:
     def __init__(self, pc, gc, angle):
         for param in (pc, gc, angle):
             if not isinstance(param , (int, float)):
-                raise TypeError(f'{param} must be an integer or a float')
+                raise TypeError(f'{param} doit être un entier ou un float')
         if not (0 < pc < gc):
             raise ValueError("0 < pc < gc n'est pas valide !!!")
+
+        if not (45 <= angle <= 90):
+            raise ValueError('angle doit etre compris entre 45 et 90')
 
         self.pc = pc 
         self.gc = gc
@@ -30,6 +33,16 @@ class Parallelo:
 
 def test_parallelo():
     """fonction de test"""
+    try:
+        p = Parallelo(10, 20)
+    except Exception as e :
+        print(e)
+
+    try:
+        p = Parallelo(10, 20, 30, 40)
+    except Exception as e :
+        print(e)
+    
     try:
         p = Parallelo(10, 20, 60)
         print(p, p.surface(), p.perimetre())
