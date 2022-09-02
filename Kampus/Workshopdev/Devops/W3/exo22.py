32fc8ef1008c4ffe1e66cc51fe28e81017a7bb8d
Duprat <yduprat@gmail.com>
2022-09-02 18:23:45 +0200

    gitautosync update 265

--- Kampus/Workshopdev/Devops/W3/exo22.py
+++ Kampus/Workshopdev/Devops/W3/exo22.py
@@ -1,9 +1,11 @@
+import sys
+
 import exo20
 
 class Losange(exo20.Parallelo):
 
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self, cote, angle):
+        super().__init__(cote, cote, angle)
         # exo20.Parallelo.__init__self, *args)
 
 def test_losange():
