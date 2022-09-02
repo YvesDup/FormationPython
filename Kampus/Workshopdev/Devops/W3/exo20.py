19e8bad961d1c8637778b61b0896bfac649d83a2
Duprat <yduprat@gmail.com>
2022-09-02 16:52:20 +0200

    gitautosync update 240

--- Kampus/Workshopdev/Devops/W3/exo20.py
+++ Kampus/Workshopdev/Devops/W3/exo20.py
@@ -8,10 +8,10 @@ class Parallelo:
         pass
 
     # conversion en string
-    def __str__(self)) -> str:
+    def __str__(self) -> str:
         return "STR"
 
-    def __repr__(self)) -> str:
+    def __repr__(self) -> str:
         return "REPR"
 
     # metodes fonctionnelles
