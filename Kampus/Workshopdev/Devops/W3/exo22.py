1a4bb814aaa02dc04e59cd55c9d98568365dd9fd
Duprat <yduprat@gmail.com>
2022-09-02 18:26:34 +0200

    gitautosync update 268

--- Kampus/Workshopdev/Devops/W3/exo22.py
+++ Kampus/Workshopdev/Devops/W3/exo22.py
@@ -8,6 +8,14 @@ class Losange(exo20.Parallelo):
         super().__init__(cote, cote, angle)
         # exo20.Parallelo.__init__self, *args)
 
+    # conversion en string
+    def __str__(self) -> str:
+        return f"LOSANGE {self.p_cote}, {self.angle}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
 def test_losange():
     """Losange Test"""
     l = Losange(50, 65)
