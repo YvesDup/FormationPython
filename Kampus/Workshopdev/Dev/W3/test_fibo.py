0a3588cb613f4d37f77b685405b5668d43f591d0
Duprat <yduprat@gmail.com>
2022-09-08 18:14:54 +0200

    gitautosync update 341

--- Kampus/Workshopdev/Dev/W3/test_fibo.py
+++ Kampus/Workshopdev/Dev/W3/test_fibo.py
@@ -10,3 +10,16 @@ def fibo(n):
 print(f'{fibo(10) = }')
 print(f'{fibo(20) = }')
 print(f'{fibo(40) = }')
+
+d = {} 
+def fibo2(n):
+    if n < 2:
+        return 1
+    if n in d:
+        return d[n]
+    d[n] = fibo2(n-1) + fibo2(n-2) 
+    return d[n]
+
+print(f'{fibo2(10) = }')
+print(f'{fibo2(20) = }')
+print(f'{fibo2(40) = }')
