f92994a15ae9826c828b5b2500bfbc08f171c64a
Duprat <yduprat@gmail.com>
2022-09-08 17:17:36 +0200

    gitautosync update 317

--- Kampus/Workshopdev/Dev/W3/exo35.py
+++ Kampus/Workshopdev/Dev/W3/exo35.py
@@ -17,7 +17,7 @@ def creer_index(phrase: str) -> Dict[str, int]:
     return dmots
 
 def test_creer_index():
-    """test c reer_index"""
+    """test creer_index"""
     try:
         assert creer_index({}) == {}
     except Exception as e:
@@ -26,6 +26,10 @@ def test_creer_index():
     assert creer_index("hello") == {'hello':1}
     assert creer_index("hello hello") == {'hello':2}
     assert creer_index("hello hello Bob") == {'hello':2, 'Bob':1}
+    s = "ton tonton est ton ami et un ami de ton ami est mon ami"
+    d = {'ami': 4, 'ton': 3, 'est': 2, 'de': 1, 'un': 1, 'mon': 1, 'et': 1, 'tonton': 1}
+    assert creer_index(s) == d
+
 
     print("Done")
 
