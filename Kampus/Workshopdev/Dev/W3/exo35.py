bb9f4dbdef67fb1133525c5769d61aba5d75698b
Duprat <yduprat@gmail.com>
2022-09-08 17:03:24 +0200

    gitautosync update 307

---
+++ Kampus/Workshopdev/Dev/W3/exo35.py
@@ -0,0 +1,12 @@
+
+
+from typing import Dict
+
+def creer_index(phrase: str) -> Dict[str, int]:
+    pass
+
+def test_creer_index():
+    """test c reer_index"""
+    assert creer_index("") == {}
+
+test_creer_index()
