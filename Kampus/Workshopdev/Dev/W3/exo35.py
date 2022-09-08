6cda672ccf6ca69d2002150110064195fdde87c4
Duprat <yduprat@gmail.com>
2022-09-08 17:11:15 +0200

    gitautosync update 312

--- Kampus/Workshopdev/Dev/W3/exo35.py
+++ Kampus/Workshopdev/Dev/W3/exo35.py
@@ -3,6 +3,9 @@
 from typing import Dict
 
 def creer_index(phrase: str) -> Dict[str, int]:
+    if not isinstance(phrase, str):
+        raise TypeError('"phrase" doit être une string')
+
     d = {}
 
     for mot in phrase.split():
@@ -12,7 +15,10 @@ def creer_index(phrase: str) -> Dict[str, int]:
 
 def test_creer_index():
     """test c reer_index"""
-    assert creer_index({}) == {}
+    try:
+        assert creer_index({}) == {}
+    except Exception as e:
+        assert isinstance(e, TypeError) 
     assert creer_index("") == {}
     assert creer_index("hello") == {'hello':1}
     print("Done")
