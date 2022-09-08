405cf5eed789769376da305c65a63c041c66b7af
Duprat <yduprat@gmail.com>
2022-09-08 15:38:52 +0200

    gitautosync update 290

---
+++ Kampus/Workshopdev/Dev/W3/exo22.py
@@ -0,0 +1,20 @@
+import exo20 as parallelo
+
+class Losange(parallelo.Parallelo):
+    """"""
+
+
+def test_losange():
+    """
+    """
+
+class Rectange(parallelo.Parallelo):
+    """"""
+
+def test_rectangle():
+    """
+    """
+
+if __name__ == '__main__':
+    test_losange()
+    test_rectangle()
