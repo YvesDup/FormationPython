f297a72b48e2d8a8f6e3fb9b5963f6a6e5d9784e
Duprat <yduprat@gmail.com>
2022-09-08 14:40:55 +0200

    gitautosync update 274

---
+++ Kampus/Workshopdev/Dev/W3/cercle.py
@@ -0,0 +1,25 @@
+class Cercle:
+    """class Cercle"""
+    def __init__(self, rayon: Number):
+        self._rayon = rayon
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {self._rayon:.03f}"
+
+    def __repr__(self) -> str:
+        return f"<{self.__str__()}>"
+
+    def perimetre(self) -> Number:
+        return 2 * self._rayon * math.pi
+
+    def surface(self) -> Number:
+        return (self._rayon ** 2) * math.pi
+
+def test_cercle():
+    """Test un cercle
+    """
+    c1 = Cercle(10)
+    print(f'{c1!s} - {c1!r}')
+    print(f'{c1.perimetre() = } - {c1.surface() = }')
+
+test_cercle()
