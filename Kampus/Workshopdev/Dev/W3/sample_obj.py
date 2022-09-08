6a0db88db6cab8f2212aa0beca91f0439681e78d
Duprat <yduprat@gmail.com>
2022-09-08 14:41:59 +0200

    gitautosync update 276

--- Kampus/Workshopdev/Dev/W3/sample_obj.py
+++ Kampus/Workshopdev/Dev/W3/sample_obj.py
@@ -67,27 +67,3 @@ from typing import Union
 
 Number = Union[float, int]
 
-class Cercle:
-    """class Cercle"""
-    def __init__(self, rayon: Number):
-        self._rayon = rayon
-
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}: {self._rayon:.03f}"
-
-    def __repr__(self) -> str:
-        return f"<{self.__str__()}>"
-
-    def perimetre(self) -> Number:
-        return 2 * self._rayon * math.pi
-
-    def surface(self) -> Number:
-        return (self._rayon ** 2) * math.pi
-
-def test_cercle():
-    """Test un cercle
-    """
-    c1 = Cercle(10)
-    print(f'{c1!s} - {c1!r} - {c1!a}')
-    print(f'{c1.perimetre() = } - {c1.surface() = }')
-test_cercle()
