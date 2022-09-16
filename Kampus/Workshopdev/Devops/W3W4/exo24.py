4c5445355e714fa89ecbee45bd4ca333f8c62350
Duprat <yduprat@gmail.com>
2022-09-16 17:29:28 +0200

    gitautosync update 428

--- Kampus/Workshopdev/Devops/W3W4/exo24.py
+++ Kampus/Workshopdev/Devops/W3W4/exo24.py
@@ -20,12 +20,19 @@ class Carre2(exo22.Rectangle):
 def test_carre():
     """
     """
+    def __str__(self) -> str:
+        return f'Carre {self.p_cote}'
+
     for cls in (Carre0, Carre1, Carre2):
         try:
             c = cls(30)
-            print(c, f'{cls.__mro__ = }, {cls.__bases__ = }')
-            print(vars(c),"vs", c.__dict__)
+            #print(c, f'{cls.__mro__ = }, {cls.__bases__ = }')
+            #print(vars(c),"vs", c.__dict__)
         except Exception as e:
             print(f'class {cls.__name__!a} -> {e}')
+        else:
+            # dynamiquement j'ai ajouté une methode
+            setattr(cls, '__str__', __str__)
+            print(c)
 
 test_carre()
