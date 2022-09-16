234dac2c9e8806c61fe36ddf9094550f149777bf
Duprat <yduprat@gmail.com>
2022-09-16 14:39:17 +0200

    gitautosync update 357

---
+++ class_cercle.py
@@ -0,0 +1,34 @@
+"""module cercle"""
+import math
+from typing import Union
+
+Number = Union[float, int]
+
+class Cercle:
+    """class Cercle"""
+    def __init__(self, rayon: Number):
+        self._rayon = rayon
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {self._rayon:.03f}"
+
+    def __repr__(self) -> str:
+        return f"<{self.__str__()}>"
+
+    def perimetre(self) -> float:
+        """xxx"""
+        return 2 * self._rayon * math.pi
+
+    def surface(self) -> float:
+        """yyy"""
+        return (self._rayon ** 2) * math.pi
+
+def test_cercle():
+    """Test un cercle
+    """
+    c1 = Cercle(10)
+    print(f'{c1!s} - {c1!r}')
+    print(f'{c1.perimetre() = } - {c1.surface() = }')
+
+if __name__ == '__main__':
+    test_cercle()
