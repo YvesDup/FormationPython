919a184850671f22b8020d89254b4a65e9837f26
Duprat <yduprat@gmail.com>
2022-09-16 16:36:39 +0200

    gitautosync update 404

--- Kampus/Workshopdev/Devops/W3W4/exo22.py
+++ Kampus/Workshopdev/Devops/W3W4/exo22.py
@@ -15,7 +15,6 @@ class Losange(exo20.Parallelo):
     def __repr__(self) -> str:
         return self.__str__()
 
-
 def test_losange():
     """Losange Test"""
     l = Losange(50, 65)
@@ -24,6 +23,19 @@ def test_losange():
     ll = [l, exo20.Parallelo(20, 30, 75)]
     print(ll)
 
+class Carre(exo20.Parallelo):
+    """class Carre herits from Parallelo"""
+
+    def __init__(self, p_cote, g_cote):
+        super().__init__(p_cote, g_cote, 90)
+        # exo20.Parallelo.__init__self, *args)
+
+    # conversion en string
+    def __str__(self) -> str:
+        return f"CARRE {self.p_cote}, {self.g_cote}"
+
+    def __repr__(self) -> str:
+        return self.__str__()
 if __name__ == "__main__":
     print(sys.version)
     test_losange() 
