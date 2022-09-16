7ae3859cdc1c704481c43d7eb9099a52793387ad
Duprat <yduprat@gmail.com>
2022-09-16 15:33:57 +0200

    gitautosync update 373

--- Kampus/Workshopdev/Devops/W3W4/class_cercle.py
+++ Kampus/Workshopdev/Devops/W3W4/class_cercle.py
@@ -37,7 +37,7 @@ def test_cercle():
     c1 = Cercle(10)
     print(f'{c1!s} - {c1!r}')
     print(f'{c1.perimetre() = } - {c1.surface() = }')
-    l1 = [c1, Cercle(5), Cercle(8), -5]
+    l1 = [c1, -5, Cercle(5), Cercle(8)]
     print(f'{c1 < Cercle(30) = }')
     print(f'{c1 < 1 = }')
     print(l1)
