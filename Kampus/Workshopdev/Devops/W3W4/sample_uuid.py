236e68a3b4fd6345e2073ed9b989063f5445f844
Duprat <yduprat@gmail.com>
2022-09-16 16:27:43 +0200

    gitautosync update 395

--- Kampus/Workshopdev/Devops/W3W4/sample_uuid.py
+++ Kampus/Workshopdev/Devops/W3W4/sample_uuid.py
@@ -5,14 +5,15 @@ x = str(uuid.uuid4())
 print(x)
 
 class Obj:
-  """ """
-  def __init__(self, name, val):
-    """ """
-    self._name = name
-    self._val = val
 
-  def get_name(self) -> str:
-    return self._name # appel à un attribut privé pour une méthode publique
+    """ """
+    def __init__(self, name, val):
+        """ """
+        self._name = name
+        self._val = val
+
+    def get_name(self) -> str:
+        return self._name # appel à un attribut privé pour une méthode publique
 
 o = Obj('Yves', 57)
 print(f"{o._name = } et {o.get_name() = }")
