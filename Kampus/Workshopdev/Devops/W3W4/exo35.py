b5efaea91e71b09ef2c154fbbca271f18b3996a9
Duprat <yduprat@gmail.com>
2022-09-16 18:07:52 +0200

    gitautosync update 445

---
+++ Kampus/Workshopdev/Devops/W3W4/exo35.py
@@ -0,0 +1,17 @@
+def creer_index(phrase: str) -> dict:
+    """
+    """
+    d = {}
+    for mot in phrase.split():
+        pass
+
+    return d
+
+def test_creer_index():
+    """
+    """
+
+    assert creer_index("") == {}
+
+if __name__ == '__main__':
+    test_creer_index()
