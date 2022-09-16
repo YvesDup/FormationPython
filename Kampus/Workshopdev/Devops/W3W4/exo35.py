aa94b7d9307b0139b09a3681856de36b68a47581
Duprat <yduprat@gmail.com>
2022-09-16 18:18:14 +0200

    gitautosync update 450

--- Kampus/Workshopdev/Devops/W3W4/exo35.py
+++ Kampus/Workshopdev/Devops/W3W4/exo35.py
@@ -6,15 +6,22 @@ def creer_index(phrase: str, occurrence_mini: int=1) -> dict:
         pass
 
     if occurrence_mini > 1:
-        return {k: v for k, v in d.items() if v > occurrence_mini}
+        return {k: v for k, v in d.items() if v >= occurrence_mini}
 
     return d
 
+def creer_index(phrase: str, occurrence_mini: int=1) -> dict:
+    """
+    """
+    pass
 def test_creer_index():
     """
     """
 
     assert creer_index("") == {}
+    assert creer_index("hello") == {"hello": 1}
+    assert creer_index("hello bye") == {"hello": 1, "bye": 1}
+    assert creer_index("hello hello") == {"hello": 2}
     assert creer_index("", 10) == {}
     print("all tests done !!!")
 
