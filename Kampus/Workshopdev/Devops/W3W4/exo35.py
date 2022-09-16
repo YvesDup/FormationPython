046e9713265fd2db98f3d948808b61b6bda6a605
Duprat <yduprat@gmail.com>
2022-09-16 18:29:07 +0200

    gitautosync update 456

--- Kampus/Workshopdev/Devops/W3W4/exo35.py
+++ Kampus/Workshopdev/Devops/W3W4/exo35.py
@@ -1,3 +1,4 @@
+import collections
 
 def creer_index(phrase: str, occurrence_mini: int=1) -> dict:
     """
@@ -17,8 +18,15 @@ def creer_index(phrase: str, occurrence_mini: int=1) -> dict:
 def creer_index1(phrase: str, occurrence_mini: int=1) -> dict:
     """
     """
+    """
+    """
+    d = collections.Counter(phrase.split())
 
-    pass
+    if occurrence_mini > 1:
+        return {k: v for k, v in d.items() if v >= occurrence_mini}
+
+    return d
+
 def test_creer_index():
     """
     """
@@ -31,8 +39,10 @@ def test_creer_index():
     s = "ton tonton est ton ami et un ami de ton ami est mon ami"
     d1 = {'ami': 4, 'ton': 3, 'est': 2, 'de': 1, 'un': 1, 'mon': 1, 'et': 1, 'tonton': 1}
     d2  ={'ami': 4, 'ton': 3}
-    assert creer_index(s) == d1
-    assert creer_index(s, 3) == d2
+    for func in (creer_index, creer_index1):
+        print(func.__name__.center(60, '.'))
+        assert func(s) == d1
+        assert func(s, 3) == d2
 
 
     print("all tests done !!!")
