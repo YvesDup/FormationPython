796f0267f9f2c350c687e44940dc160572cf2320
Duprat <yduprat@gmail.com>
2022-10-05 20:35:08 +0200

    gitautosync update 24

---
+++ Kampus/Workshopdev/W5/test_cercle.py
@@ -0,0 +1,35 @@
+import unittest
+
+import class_cercle
+
+class TestCercleInit(unittest.TestCase):
+    def test_noparam(self):
+        """ test la creatiion d'un cercle sans parametre"""
+        with self.assertRaises(TypeError):
+            class_cercle.Cercle()
+
+    def test_too_many_params(self):
+        """ test la creatiion d'un cercle sans parametre"""
+        with self.assertRaises(TypeError):
+            class_cercle.Cercle(10, 20)
+
+    def test_bad_type_param(self):
+        """ test la creatiion d'un cercle sans parametre"""
+        with self.assertRaises(TypeError):
+            class_cercle.Cercle([])
+
+    def test_bad_value_param(self):
+        """ test la creatiion d'un cercle sans parametre"""
+        with self.assertRaises(TypeError):
+            class_cercle.Cercle(-23.9)
+
+    def test_good_param(self):
+        """ test la creatiion d'un cercle sans parametre"""
+        try:
+            c = class_cercle.Cercle(10.5)
+        execpt:
+            self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
